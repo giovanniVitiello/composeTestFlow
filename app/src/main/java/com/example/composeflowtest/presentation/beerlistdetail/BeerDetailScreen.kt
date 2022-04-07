2b63c7f60a2f623863e6976bcc5c76a567d23943
Giovanni Vitiello <giovanni.vitiello10@gmail.com>
2022-04-07 15:02:08 +0200

    add second Screen

--- app/src/main/java/com/example/composeflowtest/presentation/beerlistdetail/BeerDetailScreen.kt
+++ app/src/main/java/com/example/composeflowtest/presentation/beerlistdetail/BeerDetailScreen.kt
@@ -1,6 +1,10 @@
 package com.example.composeflowtest.presentation.beerlistdetail
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.composeflowtest.domain.model.BeerDomain
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
@@ -11,5 +15,28 @@ fun BeerDetailScreen(
     navigator: DestinationsNavigator,
     beer: BeerDomain
 ) {
-
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .padding(10.dp)
+    ) {
+        Text(
+            text = "First Brewed: " + beer.firstBrewed
+        )
+        Spacer(modifier = Modifier
+            .fillMaxWidth()
+            .height(10.dp)
+            .padding(10.dp)
+        )
+        Text(
+            text = "Food Pairing: " + beer.foodPairing
+        )
+        Spacer(modifier = Modifier
+            .fillMaxWidth()
+            .height(10.dp)
+            .padding(10.dp)
+        )
+        Text(
+            text = "Brewer Tips: : " + beer.brewersTips
+        )
+    }
 }
