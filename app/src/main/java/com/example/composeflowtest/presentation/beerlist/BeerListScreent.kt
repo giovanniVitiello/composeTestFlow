682fbeb3434fcdfff80d621704037dc2eaeb6a79
Giovanni Vitiello <giovanni.vitiello10@gmail.com>
2022-04-20 17:19:41 +0200

    fix

--- app/src/main/java/com/example/composeflowtest/presentation/beerlist/BeerListScreent.kt
+++ app/src/main/java/com/example/composeflowtest/presentation/beerlist/BeerListScreent.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -12,6 +13,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -22,6 +24,7 @@ import com.example.composeflowtest.presentation.destinations.BeerDetailScreenDes
 import com.example.composeflowtest.presentation.ui.theme.ComposeFlowTestTheme
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.skydoves.landscapist.glide.GlideImage
 
 @Destination(start = true)
 @Composable
@@ -30,7 +33,9 @@ fun BeerListScreen(
     viewModel: MainViewModel = hiltViewModel()
 ) {
     val state = viewModel.stateFlow.collectAsState()
-    Surface(modifier = Modifier.fillMaxWidth().fillMaxHeight()) {
+    Surface(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxHeight()) {
         LazyColumn {
             items(items = state.value.beers) { beer ->
                 Item(beer = beer, onClick = {
@@ -49,10 +54,28 @@ fun Item(beer: BeerDomain, onClick: (BeerDomain) -> Unit) {
             .clickable { onClick(beer) }
     ) {
         Row(Modifier.fillMaxWidth()) {
-            Image(
-                painter = rememberAsyncImagePainter(beer.imageUrl),
-                contentDescription = null,
-                modifier = Modifier.size(128.dp)
+            GlideImage(
+                imageModel = beer.imageUrl,
+                // Crop, Fit, Inside, FillHeight, FillWidth, None
+                contentScale = ContentScale.FillHeight,
+                modifier = Modifier.size(128.dp),
+                // shows a placeholder ImageBitmap when loading.
+//                placeHolder = ImageBitmap.imageResource(R.drawable.placeholder),
+                // shows an error ImageBitmap when the request failed.
+//                error = ImageBitmap.imageResource(R.drawable.error)
+
+                // shows an indicator while loading an image.
+                loading = {
+                    Box(modifier = Modifier.matchParentSize()) {
+                        CircularProgressIndicator(
+                            modifier = Modifier.align(Alignment.Center)
+                        )
+                    }
+                },
+                // shows an error text if fail to load an image.
+                failure = {
+                    Text(text = "image request failed.")
+                }
             )
             Column(
                 modifier = Modifier
